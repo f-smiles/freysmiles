afc34df51ac0ec39f5d0db3dd97cafd8eaebbf23
Wanyi <wanyi.ng1975@gmail.com>
2024-03-21 14:52:30 -0400

    wip

---
+++ app/test/[productId]/loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <section className="container mx-auto text-center">
+      <p>Loading Single Product...</p>
+    </section>
+  )
+}
---
+++ app/test/page.js
@@ -0,0 +1,36 @@
+import { Suspense } from "react"
+import ProductsComponent from "."
+import Loading from "./loading"
+
+export const metadata = {
+  title: "TEST Products",
+}
+
+async function getProducts() {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/products`)
+    return res.json()
+  } catch (error) {
+    throw new Error(error.message)
+  }
+}
+
+async function getPrices() {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/prices`)
+    return res.json()
+  } catch (error) {
+    throw new Error(error.message)
+  }
+}
+
+export default async function Page() {
+  const products = await getProducts()
+  const prices = await getPrices()
+
+  return (
+    <Suspense fallback={<Loading />}>
+      <ProductsComponent products={products} prices={prices} />
+    </Suspense>
+  )
+}
