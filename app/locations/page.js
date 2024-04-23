c444f32755d89bcd13085a962db192dbc468d109
Wanyi Ng <wanyi.ng1975@gmail.com>
2024-04-23 14:23:34 -0400

    more Locations styling

--- app/locations/page.js
+++ app/locations/page.js
@@ -93,20 +93,22 @@ export default function Test() {
         --white: white;
         --rosemary: #147b5d;
         --rosemary-text: #fee5e1;
+        bg-[#ff6432]
         ```
       */}
-      <section className="bg-[#ff6432]">
+      <section className="bg-[#f8f1de]">
         <div id="locations-heading" className="relative block max-w-2xl px-4 py-16 mx-auto sm:px-6 sm:py-24 lg:max-w-[100rem] lg:px-8 lg:py-32">
           <h1 className="lg:text-6xl font-agrandir-bold text-[#171616]">
             Come see us at any of our{" "}
-            <span className="relative text-[#fee5e1] leading-tight lowercase font-editorial-new underline decoration-wavy decoration-[#171616] underline-offset-8">
+            <span className="relative inline-block my-8 leading-tight lowercase font-editorial-new underline-offset-8">
               four convenient locations
+              <img className="absolute w-full h-auto -ml-2 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" src="/../../images/ellipse.svg" />
             </span>{" "}
             or opt for a{" "}
-            <span className="relative text-[#fee5e1] leading-tight lowercase font-editorial-new decoration-wavy underline-offset-8 decoration-[#171616] underline">virtual consultation</span>
+            <span className="relative leading-tight lowercase font-editorial-new decoration-wavy underline-offset-8 decoration-[#147b5d] underline inline-block">virtual consultation</span>
           </h1>
-          <svg className="absolute bottom-0 translate-y-1/2 left-0 translate-x-64 w-36 h-36 text-white rotate-[120deg]" viewBox="0 0 77 85" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1.33755 84.3973C0.297616 62.7119 2.93494 39.8181 19.4192 23.8736C28.2211 15.3599 42.4944 12.5723 47.6281 26.2359C51.1245 35.5419 51.542 51.9945 41.0605 57.0865C29.486 62.7095 40.2945 35.2221 41.9942 32.4952C49.9497 19.7313 59.7772 11.6122 72.2699 3.78281C76.9496 0.849879 73.7108 0.477284 70.0947 1.13476C66.9572 1.7052 63.4035 2.43717 60.5291 3.81975C59.6524 4.24143 65.7349 2.73236 66.6827 2.44768C70.7471 1.22705 75.4874 -0.0219285 75.9527 5.60812C76.0274 6.5127 75.9956 14.9844 74.7481 15.2963C74.099 15.4586 71.0438 10.27 70.4642 9.65288C66.6996 5.64506 63.5835 4.42393 58.2726 5.11792" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+          <svg className="absolute bottom-0 translate-y-1/2 left-0 translate-x-64 w-36 h-36 rotate-[120deg] text-[#ff6432]" viewBox="0 0 77 85" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1.33755 84.3973C0.297616 62.7119 2.93494 39.8181 19.4192 23.8736C28.2211 15.3599 42.4944 12.5723 47.6281 26.2359C51.1245 35.5419 51.542 51.9945 41.0605 57.0865C29.486 62.7095 40.2945 35.2221 41.9942 32.4952C49.9497 19.7313 59.7772 11.6122 72.2699 3.78281C76.9496 0.849879 73.7108 0.477284 70.0947 1.13476C66.9572 1.7052 63.4035 2.43717 60.5291 3.81975C59.6524 4.24143 65.7349 2.73236 66.6827 2.44768C70.7471 1.22705 75.4874 -0.0219285 75.9527 5.60812C76.0274 6.5127 75.9956 14.9844 74.7481 15.2963C74.099 15.4586 71.0438 10.27 70.4642 9.65288C66.6996 5.64506 63.5835 4.42393 58.2726 5.11792" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
           </svg>
         </div>
 
@@ -152,8 +154,8 @@ export default function Test() {
               >
                 <button
                   className={`${
-                    selectedLocation === "All" ? "text-white " : ""
-                  } self-end transition-all duration-300 ease-linear w-max  hover:text-secondary-50 mr-6 mb-6`}
+                    selectedLocation === "All" ? "text-[#147b5d]" : ""
+                  } self-end transition-all duration-300 ease-linear w-max mr-6 mb-6 underline underline-offset-4 hover:text-[#147b5d]`}
                   onClick={() => setSelectedLocation("All")}
                 >
                   {selectedLocation === "All"
@@ -161,7 +163,7 @@ export default function Test() {
                     : "Show All Locations"}
                 </button>
 
-                <dl ref={scope} className="divide-y">
+                <dl ref={scope} className="divide-y divide-[#147b5d]">
                   {locations.map((l, i) => (
                     <Disclosure
                       as="div"
