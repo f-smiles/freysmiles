0e10b7d6b849c7845b7a5559d910bcc1d50ff68b
Wanyi Ng <wanyi.ng1975@gmail.com>
2024-05-11 21:23:17 -0400

    remove fragment

--- app/app.js
+++ app/app.js
@@ -7,14 +7,12 @@ import Footer from './_components/Footer'
 
 export default function App({ children }) {
   return (
-    <>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <Navbar />
-          {children}
-          <Footer />
-        </PersistGate>
-      </Provider>
-    </>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Navbar />
+        {children}
+        <Footer />
+      </PersistGate>
+    </Provider>
   )
 }
