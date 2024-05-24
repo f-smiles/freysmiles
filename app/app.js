7c9441ab9a9e24070030cf67ed2711a113807859
Wanyi Ng <wanyi.ng1975@gmail.com>
2024-05-24 00:04:32 -0400

    scroll reveal footer animation

--- app/app.js
+++ app/app.js
@@ -1,4 +1,6 @@
 'use client'
+import { useEffect } from 'react'
+import Lenis from '@studio-freight/lenis'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { store, persistor } from './_store/config'
@@ -6,6 +8,17 @@ import Navbar from './_components/Navbar'
 import Footer from './_components/Footer'
 
 export default function App({ children }) {
+  useEffect( () => {
+    const lenis = new Lenis()
+
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+  }, [])
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
