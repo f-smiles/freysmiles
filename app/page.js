963f85687e710d48cc8c4cadcf59316004836bb5
Jo <jolee224@gmail.com>
2024-05-26 10:43:50 -0400

    background

--- app/page.js
+++ app/page.js
@@ -250,7 +250,7 @@ export default function LandingComponent() {
 
 function LogoHeader() {
   return (
-    <header className="pt-16 m-auto w-max">
+    <header className=" m-auto w-max">
       {/* <div className="bg-[rgba(253,_192,_129,_1)] rounded-full shadow-[0px_0px_0px_8px_rgba(253,_192,_129,_0.8),_0px_0px_0px_16px_rgba(253,_199,_143,0.6),_0px_0px_0px_24px_rgba(253,_206,_157,_0.4),_0px_0px_0px_32px_rgba(253,_213,_171,_0.2),_0px_0px_0px_40px_rgba(254,_220,_185,_0.1)]">
         <img
           className="w-16 h-16 p-4"
@@ -580,167 +580,170 @@ function Hero() {
   const canvasRef = useRef(null);
   const obj = useRef({ trans: 0 });
   
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    canvas.style.width = 1024
-    const gl = canvas.getContext('webgl');
-    let cnt = 0;
-    let textureArr = [];
+  // useEffect(() => {
+  //   const canvas = canvasRef.current;
+  //   canvas.style.width = 1024
+  //   const gl = canvas.getContext('webgl');
+  //   let cnt = 0;
+  //   let textureArr = [];
   
-    let program = gl.createProgram();
-  
-  
-    const vShader = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vShader, vertexSrc);
-    gl.compileShader(vShader);
-  
-    const fShader = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fShader, fragmentSrc);
-    gl.compileShader(fShader);
+  //   let program = gl.createProgram();
   
   
-    gl.attachShader(program, vShader);
-    gl.deleteShader(vShader);
-    gl.attachShader(program, fShader);
-    gl.deleteShader(fShader);
-    gl.linkProgram(program);
+  //   const vShader = gl.createShader(gl.VERTEX_SHADER);
+  //   gl.shaderSource(vShader, vertexSrc);
+  //   gl.compileShader(vShader);
   
-    const vertices = new Float32Array([
-      -1, -1,
-      1, -1,
-      -1, 1,
-      1, -1,
-      -1, 1,
-      1, 1,
-    ]);
-  
-    const vertexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-    const vertexLocation = gl.getAttribLocation(program, 'position');
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+  //   const fShader = gl.createShader(gl.FRAGMENT_SHADER);
+  //   gl.shaderSource(fShader, fragmentSrc);
+  //   gl.compileShader(fShader);
   
   
-    const uTransLoc = gl.getUniformLocation(program, 'uTrans');
-    const textureLocArr = [
-      gl.getUniformLocation(program, 'uTexture0'),
-      gl.getUniformLocation(program, 'uTexture1'),
-      gl.getUniformLocation(program, 'uDisp')
-    ];
+  //   gl.attachShader(program, vShader);
+  //   gl.deleteShader(vShader);
+  //   gl.attachShader(program, fShader);
+  //   gl.deleteShader(fShader);
+  //   gl.linkProgram(program);
   
-    const obj = { trans: 0 };
+  //   const vertices = new Float32Array([
+  //     -1, -1,
+  //     1, -1,
+  //     -1, 1,
+  //     1, -1,
+  //     -1, 1,
+  //     1, 1,
+  //   ]);
   
-    function start() {
-      loop();
-    }
+  //   const vertexBuffer = gl.createBuffer();
+  //   gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  //   gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+  //   const vertexLocation = gl.getAttribLocation(program, 'position');
+  //   gl.bindBuffer(gl.ARRAY_BUFFER, null);
   
-    function loop() {
-      gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-      gl.clearColor(0.0, 0.0, 0.0, 1.0);
-      gl.clear(gl.COLOR_BUFFER_BIT);
   
-      gl.useProgram(program);
+  //   const uTransLoc = gl.getUniformLocation(program, 'uTrans');
+  //   const textureLocArr = [
+  //     gl.getUniformLocation(program, 'uTexture0'),
+  //     gl.getUniformLocation(program, 'uTexture1'),
+  //     gl.getUniformLocation(program, 'uDisp')
+  //   ];
   
-      gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-      gl.vertexAttribPointer(
-        vertexLocation, 2, gl.FLOAT, false, 0, 0)
-      gl.enableVertexAttribArray(vertexLocation);
+  //   const obj = { trans: 0 };
   
-      textureArr.forEach((texture, index) => {
-        gl.activeTexture(gl.TEXTURE0 + index);
-        gl.bindTexture(gl.TEXTURE_2D, texture);
-        gl.uniform1i(textureLocArr[index], index);
-      })
+  //   function start() {
+  //     loop();
+  //   }
   
-      gl.uniform1f(uTransLoc, obj.trans);
+  //   function loop() {
+  //     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  //     gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  //     gl.clear(gl.COLOR_BUFFER_BIT);
   
-      gl.drawArrays(gl.TRIANGLES, 0, 6);
+  //     gl.useProgram(program);
   
-      requestAnimationFrame(loop);
-    }
+  //     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  //     gl.vertexAttribPointer(
+  //       vertexLocation, 2, gl.FLOAT, false, 0, 0)
+  //     gl.enableVertexAttribArray(vertexLocation);
   
-    function resize() {
-      const aspectRatio = 1 / 2; 
-      const maxWidth = 512; 
-      const minHeight = 300; 
+  //     textureArr.forEach((texture, index) => {
+  //       gl.activeTexture(gl.TEXTURE0 + index);
+  //       gl.bindTexture(gl.TEXTURE_2D, texture);
+  //       gl.uniform1i(textureLocArr[index], index);
+  //     })
+  
+  //     gl.uniform1f(uTransLoc, obj.trans);
+  
+  //     gl.drawArrays(gl.TRIANGLES, 0, 6);
+  
+  //     requestAnimationFrame(loop);
+  //   }
+  
+  //   function resize() {
+  //     const aspectRatio = 1 / 2; 
+  //     const maxWidth = 512; 
+  //     const minHeight = 300; 
 
-      let width = Math.min(window.innerWidth, window.innerHeight * aspectRatio);
-      let height = width / aspectRatio;
+  //     let width = Math.min(window.innerWidth, window.innerHeight * aspectRatio);
+  //     let height = width / aspectRatio;
 
-      if (height < minHeight) {
-          height = minHeight;
-          width = height * aspectRatio;
-      }
+  //     if (height < minHeight) {
+  //         height = minHeight;
+  //         width = height * aspectRatio;
+  //     }
   
-      if (width > maxWidth) {
-          width = maxWidth;
-          height = width / aspectRatio;
-      }
+  //     if (width > maxWidth) {
+  //         width = maxWidth;
+  //         height = width / aspectRatio;
+  //     }
   
-      canvas.width = width;
-      canvas.height = height;
-  }
+  //     canvas.width = width;
+  //     canvas.height = height;
+  // }
   
   
-    function loadImages() {
-      assetUrls.forEach ((url, index) => {
-        let img = new Image();
+  //   function loadImages() {
+  //     assetUrls.forEach ((url, index) => {
+  //       let img = new Image();
   
-        let texture = gl.createTexture();
-        textureArr.push(texture);
+  //       let texture = gl.createTexture();
+  //       textureArr.push(texture);
   
-   img.onload =  function (_index, _img) {
-          let texture = textureArr[_index];
+  //  img.onload =  function (_index, _img) {
+  //         let texture = textureArr[_index];
   
-          gl.bindTexture(gl.TEXTURE_2D, texture);
-          gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, _img);
-          gl.generateMipmap(gl.TEXTURE_2D);
+  //         gl.bindTexture(gl.TEXTURE_2D, texture);
+  //         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, _img);
+  //         gl.generateMipmap(gl.TEXTURE_2D);
   
-          cnt++;
-          if (cnt === 3) start();
-        }.bind(this, index, img);
+  //         cnt++;
+  //         if (cnt === 3) start();
+  //       }.bind(this, index, img);
   
-        img.crossOrigin = ' Anonymous';
-        img.src = url;
+  //       img.crossOrigin = ' Anonymous';
+  //       img.src = url;
      
-        console.log(img)
-      });
-    }
+  //       console.log(img)
+  //     });
+  //   }
   
   
-    canvas.addEventListener('mouseenter', () => {
-      gsap.killTweensOf(obj);
-      gsap.to(obj, 1.5, { trans: 1 });
-    });
+  //   canvas.addEventListener('mouseenter', () => {
+  //     gsap.killTweensOf(obj);
+  //     gsap.to(obj, 1.5, { trans: 1 });
+  //   });
   
-    canvas.addEventListener('mouseleave', () => {
-      gsap.killTweensOf(obj);
-      gsap.to(obj, 1.5, { trans: 0 });
-    });
+  //   canvas.addEventListener('mouseleave', () => {
+  //     gsap.killTweensOf(obj);
+  //     gsap.to(obj, 1.5, { trans: 0 });
+  //   });
   
-    window.addEventListener('resize', () => {
-      resize();
-    });
+  //   window.addEventListener('resize', () => {
+  //     resize();
+  //   });
   
-    loadImages();
-    resize();
+  //   loadImages();
+  //   resize();
   
-    return () => {
-      window.removeEventListener('resize', resize);
-      canvas.removeEventListener('mouseenter', () => {
-        gsap.killTweensOf(obj);
-        gsap.to(obj, 1.5, { trans: 1 });
-      });
-      canvas.removeEventListener('mouseleave', () => {
-        gsap.killTweensOf(obj);
-        gsap.to(obj, 1.5, { trans: 0 });
-      });
-    };
-  }, []);
+  //   return () => {
+  //     window.removeEventListener('resize', resize);
+  //     canvas.removeEventListener('mouseenter', () => {
+  //       gsap.killTweensOf(obj);
+  //       gsap.to(obj, 1.5, { trans: 1 });
+  //     });
+  //     canvas.removeEventListener('mouseleave', () => {
+  //       gsap.killTweensOf(obj);
+  //       gsap.to(obj, 1.5, { trans: 0 });
+  //     });
+  //   };
+  // }, []);
   
   return (
 
-    <section className="relative mt-6">
+    <section
+    className="relative bg-cover bg-center"
+    style={{ backgroundImage: 'url(../images/purplepeach.jpg)' }}
+  >
 
       <div ref={pixiContainerRef} id="pixi-container"></div>
       <div className="px-8 isolate lg:px-8">
