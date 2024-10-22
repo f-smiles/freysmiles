b55661418cf1b401c49b29cbe124ef7d10cdc77b
Wanyi Ng <wanyi.ng1975@gmail.com>
2024-10-21 20:36:22 -0400

    edit and delete product from admin dashboard

---
+++ app/dashboard/edit-product/page.tsx
@@ -0,0 +1,7 @@
+import EditProductForm from "./edit-form";
+
+export default function EditProduct() {
+  return (
+    <EditProductForm />
+  )
+}
---
+++ server/actions/edit-product.ts
@@ -0,0 +1,35 @@
+'use server'
+
+import { revalidatePath } from "next/cache"
+import { eq } from "drizzle-orm"
+
+import { actionClient } from "@/lib/safe-action"
+import { ProductSchema } from "@/types/product-schema"
+import { db } from "@/server/db"
+import { products } from "@/server/schema"
+
+
+export const editProduct = actionClient
+  .schema(ProductSchema)
+  .action(async ({ parsedInput: { id, title, description, price } }) => {
+    try {
+      const existingProduct = await db.query.products.findFirst({
+        where: eq(products.id, id),
+      })
+
+      if (!existingProduct) return { error: "Product does not exist." }
+
+      if (existingProduct) {
+        const updatedProduct = await db
+          .update(products)
+          .set({ title, description, price })
+          .where(eq(products.id, existingProduct.id))
+          .returning()
+
+        revalidatePath("/dashboard/products")
+        return { success: `Product ${updatedProduct[0].title} was updated successfully!` }
+      }
+    } catch (error) {
+      return { error: "Something went wrong when attempting to update the product. Please try again." }
+    }
+  })
