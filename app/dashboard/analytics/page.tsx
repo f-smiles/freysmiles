8703bfc640eabb8a730dee7653e9cd7173eaf855
Wanyi Ng <wanyi.ng1975@gmail.com>
2024-11-05 23:41:49 -0500

    admin analytics page

---
+++ app/dashboard/analytics/page.tsx
@@ -0,0 +1,43 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { db } from "@/server/db"
+import { orderItem } from "@/server/schema"
+import { desc } from "drizzle-orm"
+import Sales from "./sales"
+
+
+export default async function DashboardAnalytics() {
+  const totalOrders = await db.query.orderItem.findMany({
+    orderBy: [desc(orderItem.id)],
+    limit: 10,
+    with: {
+      order: { with: { user: true } },
+      product: true,
+      productVariant: { with: { variantImages: true }},
+    }
+  })
+
+  if (totalOrders.length === 0) {
+    return (
+      <Card>
+        <CardHeader>
+          <CardTitle>No orders yet.</CardTitle>
+          <CardDescription></CardDescription>
+        </CardHeader>
+      </Card>
+    )
+  }
+
+  if (totalOrders.length > 0) {
+    return (
+      <Card>
+        <CardHeader>
+          <CardTitle>Your Analytics</CardTitle>
+          <CardDescription>Check your sales, customers, and more</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Sales totalOrders={totalOrders} />
+        </CardContent>
+      </Card>
+    )
+  }
+}
