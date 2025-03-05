0f5cf542bbd58e98d186f8da65efecb986165a1b
Wanyi Ng <wanyi.ng1975@gmail.com>
2025-03-05 13:54:12 -0500

    import main nav

--- app/app.js
+++ app/app.js
@@ -1,20 +1,17 @@
 'use client'
-import { useEffect } from 'react'
-
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { store, persistor } from './_store/config'
-import Navbar from './_components/Navbar'
+import Navbar from '@/components/nav/main-nav'
 import Footer from './_components/Footer'
 import { Toast } from '@/components/ui/toaster'
 
 
 export default function App({ children, user }) {
-
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        {/* <Navbar user={user} /> */}
+        <Navbar user={user} />
         <main>
           <Toast />
           {children}
