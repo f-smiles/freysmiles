1026916f1ea84e9355c46002e33173c1e4dae0fd
Wanyi Ng <wanyi.ng1975@gmail.com>
2025-03-05 14:13:53 -0500

    setup next-themes and theme provider

--- app/app.js
+++ app/app.js
@@ -5,19 +5,21 @@ import { store, persistor } from './_store/config'
 import Navbar from '@/components/nav/main-nav'
 import Footer from './_components/Footer'
 import { Toast } from '@/components/ui/toaster'
-
+import NextThemeProvider from '@/components/providers/theme-provider'
 
 export default function App({ children, user }) {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Navbar user={user} />
-        <main>
-          <Toast />
-          {children}
-        </main>
-        <Footer />
-      </PersistGate>
-    </Provider>
+    <NextThemeProvider>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Navbar user={user} />
+          <main>
+            <Toast />
+            {children}
+          </main>
+          <Footer />
+        </PersistGate>
+      </Provider>
+    </NextThemeProvider>
   )
 }
