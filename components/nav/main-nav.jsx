970841c20cc7e7674ffa095ae279f091358352b3
Wanyi Ng <wanyi.ng1975@gmail.com>
2025-03-05 14:14:20 -0500

    toggle theme buttons

--- components/nav/main-nav.jsx
+++ components/nav/main-nav.jsx
@@ -1,11 +1,13 @@
 import DesktopNav from '@/components/nav/desktop-nav'
 import MobileNav from '@/components/nav/mobile-nav'
+import ThemeButtons from '@/components/nav/theme-buttons'
 
 export default function Navbar({ user }) {
   return (
-    <>
+    <div className="relative">
       <DesktopNav user={user} />
       <MobileNav user={user} />
-    </>
+      <ThemeButtons />
+    </div>
   )
 }
