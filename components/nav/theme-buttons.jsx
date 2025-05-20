825f45b0576a48b2082b5a1dc12d353e8e59358b
Wanyi Ng <wanyi.ng1975@gmail.com>
2025-05-19 21:53:14 -0400

    temp fix missing import

--- components/nav/theme-buttons.jsx
+++ components/nav/theme-buttons.jsx
@@ -181,38 +181,38 @@ const Clouds = () => {
   )
 }
 
-const ThemeButtonsOld = () => {
-  const { theme, setTheme } = useTheme()
+// const ThemeButtonsOld = () => {
+//   const { theme, setTheme } = useTheme()
 
-  return (
-    <div className="fixed bottom-[24px] right-[24px]">
-      <Button
-        className={cn(
-          "size-6 px-0 aspect-square rounded-full bg-transparent text-primary/50 hover:bg-primary/25 hover:text-primary/75",
-          theme === "light" ? "bg-primary/15 text-primary/100" : ""
-        )}
-        onClick={() => { setTheme("light") }}
-      >
-        <SunIcon className="h-[1.2rem] w-[1.2rem]" />
-      </Button>
-      <Button
-        className={cn(
-          "size-6 px-0 aspect-square rounded-full bg-transparent text-primary/50 hover:bg-primary/25 hover:text-primary/75",
-          theme === "system" ? "bg-primary/15 text-primary/100" : ""
-        )}
-        onClick={() => { setTheme("system") }}
-      >
-        <MonitorIcon className="h-[1.2rem] w-[1.2rem]" />
-      </Button>
-      <Button
-        className={cn(
-          "size-6 px-0 aspect-square rounded-full bg-transparent text-primary/50 hover:bg-primary/25 hover:text-primary/75",
-          theme === "dark" ? "bg-primary/15 text-primary/100" : ""
-        )}
-        onClick={() => { setTheme("dark") }}
-      >
-        <MoonIcon className="h-[1.2rem] w-[1.2rem]" />
-      </Button>
-    </div>
-  )
-}
+//   return (
+//     <div className="fixed bottom-[24px] right-[24px]">
+//       <Button
+//         className={cn(
+//           "size-6 px-0 aspect-square rounded-full bg-transparent text-primary/50 hover:bg-primary/25 hover:text-primary/75",
+//           theme === "light" ? "bg-primary/15 text-primary/100" : ""
+//         )}
+//         onClick={() => { setTheme("light") }}
+//       >
+//         <SunIcon className="h-[1.2rem] w-[1.2rem]" />
+//       </Button>
+//       <Button
+//         className={cn(
+//           "size-6 px-0 aspect-square rounded-full bg-transparent text-primary/50 hover:bg-primary/25 hover:text-primary/75",
+//           theme === "system" ? "bg-primary/15 text-primary/100" : ""
+//         )}
+//         onClick={() => { setTheme("system") }}
+//       >
+//         <MonitorIcon className="h-[1.2rem] w-[1.2rem]" />
+//       </Button>
+//       <Button
+//         className={cn(
+//           "size-6 px-0 aspect-square rounded-full bg-transparent text-primary/50 hover:bg-primary/25 hover:text-primary/75",
+//           theme === "dark" ? "bg-primary/15 text-primary/100" : ""
+//         )}
+//         onClick={() => { setTheme("dark") }}
+//       >
+//         <MoonIcon className="h-[1.2rem] w-[1.2rem]" />
+//       </Button>
+//     </div>
+//   )
+// }
