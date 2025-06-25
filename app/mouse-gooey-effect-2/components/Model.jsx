dd5fe9b3d793584cda5383c04ffcb97cc557a77f
Wanyi Ng <wanyi.ng1975@gmail.com>
2025-06-25 16:42:38 -0400

    delete ununused files

--- app/mouse-gooey-effect-2/components/Model.jsx
+++
@@ -1,9 +0,0 @@
-import vertexShader from '../glsl/vertexShader.glsl'
-import fragmentShader from '../glsl/fragmentShader.glsl'
-
-export default function Model() {
-  
-  return (
-    <mesh></mesh>
-  )
-}
