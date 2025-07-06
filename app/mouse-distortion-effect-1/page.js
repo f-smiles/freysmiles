f831a9725366b72c0239f455b6ec6ecf630dd666
Wanyi Ng <wanyi.ng1975@gmail.com>
2025-07-06 14:11:26 -0400

    add grid overlay

---
+++ app/mouse-distortion-effect-1/page.js
@@ -0,0 +1,12 @@
+import './css/style.css'
+import GridOverlay from './components/GridOverlay'
+import GridContainer from './components/GridContainer'
+
+export default function Page() {
+  return (
+    <>
+      <GridOverlay />
+      <GridContainer />
+    </>
+  )
+}
