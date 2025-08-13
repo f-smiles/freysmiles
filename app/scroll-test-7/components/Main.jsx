65c2c77bdea553ae4c164058fc2ba2fee23f5706
Wanyi Ng <wanyi.ng1975@gmail.com>
2025-08-13 10:12:43 -0400

    cleanup code

--- app/scroll-test-7/components/Main.jsx
+++ app/scroll-test-7/components/Main.jsx
@@ -1,8 +1,6 @@
 'use client'
-import React, { useEffect, useRef } from 'react'
-import { motion, useScroll, useTransform } from 'motion/react'
+import { useEffect, useRef } from 'react'
 import gsap from 'gsap'
-import { data } from '../js/data.js'
 
 export function Main() {
   const mainSection = useRef(null)
@@ -12,7 +10,6 @@ export function Main() {
   useEffect(() => {
     const items = document.querySelectorAll('.MainSectionItem')
     const innerItems = document.querySelectorAll('.MainSectionItem-inner')
-    const innerStickies = document.querySelectorAll('.MainSectionItem-innerSticky')
     const imageContainers = document.querySelectorAll('.MainSectionItem-imageContainer')
     const imageContainersInner = document.querySelectorAll('.MainSectionItem-imageContainerInner')
     const images = document.querySelectorAll('.MainSectionItem-image')
@@ -34,7 +31,6 @@ export function Main() {
       // blue
       gsap.set(items[0], { xPercent: 0 })
       gsap.set(innerItems[0], { xPercent: 0 })
-      gsap.set(innerStickies[0], { xPercent: 0 })
       gsap.set(imageContainers[0], { xPercent: -60, transformOrigin: '100% 100% 0px' })
       gsap.set(imageContainersInner[0], { xPercent: 0, transformOrigin: '50% 50% 0px' })
       gsap.set(images[0], { aspectRatio: 1.3793103448275863 })
@@ -75,10 +71,10 @@ export function Main() {
       gsap.set(imageContainersInner[3], { xPercent: 0, scale: 2, transformOrigin: '50% 50% 0px' })
       gsap.set(images[3], { aspectRatio: 1.3793103448275863 })
       
-      tl.to(items[3], { translateX: '-100%', duration: 16 }, '<') // '-99.9588%'
-      tl.to(innerItems[3], { translateX: '100%', duration: 16 }, '<') // '99.9588%'
-      tl.to(imageContainers[3], { translateX: '-80%', scale: 0.8, duration: 12 }, '<') // -149.963% 0.8001
-      tl.to(imageContainersInner[3], { translateX: '0%', scale: 1.2, duration: 12 }, '<') // 1.1999
+      tl.to(items[3], { translateX: '-100%', duration: 16 }, '<')
+      tl.to(innerItems[3], { translateX: '100%', duration: 16 }, '<')
+      tl.to(imageContainers[3], { translateX: '-80%', scale: 0.8, duration: 12 }, '<')
+      tl.to(imageContainersInner[3], { translateX: '0%', scale: 1.2, duration: 12 }, '<')
       
       // terra
       gsap.set(items[4], { xPercent: 100 })
@@ -87,9 +83,9 @@ export function Main() {
       gsap.set(imageContainersInner[4], { xPercent: 0, scale: 2, transformOrigin: '50% 50% 0px' })
       gsap.set(images[4], { aspectRatio: 1.3793103448275863 })
 
-      tl.to(items[4], { translateX: '-100%', duration: 32 }, '<') // '0.0001%'
-      tl.to(innerItems[4], { translateX: '100%', duration: 32 }, '<') // '0.0001%'
-      tl.to(imageContainers[4], { translateX: '-80%', scale: 0.8, duration: 24 }, '<') // -59.9999%
+      tl.to(items[4], { translateX: '-100%', duration: 32 }, '<')
+      tl.to(innerItems[4], { translateX: '100%', duration: 32 }, '<')
+      tl.to(imageContainers[4], { translateX: '-80%', scale: 0.8, duration: 24 }, '<')
       tl.to(imageContainersInner[4], { translateX: '0%', scale: 1.2,  duration: 24 }, '<')
 
       gsap.set(markerLeft.current, { left: mainSection.current.clientWidth * 0.33 })
@@ -102,7 +98,7 @@ export function Main() {
 
   return (
     <>
-      <div ref={mainSection} className="MainSection --dark --in-view" style={{ backgroundColor: 'var(--blue)', '--91c5acce': 5, }}>
+      <div ref={mainSection} className="MainSection` --dark --in-view" style={{ backgroundColor: 'var(--blue)', '--91c5acce': 5, }}>
         <div className="MainSection-wrap" style={{ position: 'sticky' }}>
           <div className="MainSection-items">
             <div className="MainSectionItem MainSection-item">
@@ -228,129 +224,8 @@ export function Main() {
           </div>
         </div>
       </div>
-      <div ref={markerLeft} className="custom-marker left"></div>
-      <div ref={markerRight} className="custom-marker right"></div>
+      {/* <div ref={markerLeft} className="custom-marker left"></div> */}
+      {/* <div ref={markerRight} className="custom-marker right"></div> */}
     </>
   )
 }
-
-const Item = ({ backgroundColor, index, contentTitle, contentText, imgUrl, children }) => {
-  const ref = useRef(null)
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["end end", "start start"],
-  })
-
-  const animateX = useTransform(scrollYProgress, [0, 1], [100, 0])
-
-  return (
-    <motion.div ref={ref} style={{ transform: `translate3d(${animateX}, '0px', '0px')` }} className="MainSectionItem MainSection-item">
-      <div className="--index-first MainSectionItem-inner">
-        <div className="MainSectionItem-innerSticky">
-          <div className="MainSectionItem-background" style={{ backgroundColor }} />
-          <div className="MainSectionItem-content">
-            <span className="MainSectionItem-index">{index}</span>
-            <h3 className="MainSectionItem-contentTitle AppTitle-3">{contentTitle}</h3>
-            <div className="MainSectionItem-contentText">
-              <div className="AppText-12">
-                <p>{contentText}</p>
-              </div>
-            </div>
-          </div>
-          <div className="MainSectionItem-imageContainer">
-            <div className="MainSectionItem-imageContainerInner">
-              <div className="AppImage MainSectionItem-image">
-                <div className="AppImage-image --placeholder --lazy --loaded" style={{ objectFit: 'cover', backgroundImage: imgUrl, backgroundSize: 'cover', backgroundPosition: 'center center', }} ></div>
-                <img src={imgUrl} width="480" height="348" alt="Video of a landscape" loading="lazy" style={{ objectFit: 'cover' }} />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </motion.div>
-  )
-}
-
- {/* {data.map((_) => (
-    <Item
-      backgroundColor={_.backgroundColor}
-      index={_.id}
-      contentTitle={_.heading}
-      contentText={_.description}
-      backgroundImage={"/images/test/1.jpg"}
-      imgUrl={"/images/test/1.jpg"}
-    />
-  ))} */}
-
-//  <div className="MainSectionItem MainSection-item">
-//   <div className="--index-first MainSectionItem-inner">
-//     <div className="MainSectionItem-innerSticky">
-//       <div className="MainSectionItem-background" style={{ backgroundColor: 'var(--blue)', }} />
-//       <div className="MainSectionItem-content">
-//         <span className="MainSectionItem-index">01</span>
-//         <h3 className="MainSectionItem-contentTitle AppTitle-3">Wildlife in Action: A Glimpse into Nature's Daily Drama</h3>
-//         <div className="MainSectionItem-contentText">
-//           <div className="AppText-12 ">
-//             <p>Explore the untouched beauty of forests, mountains, and rivers as we uncover the hidden secrets of nature's most breathtaking landscapes.</p>
-//           </div>
-//         </div>
-//       </div>
-//       <div className="MainSectionItem-imageContainer">
-//         <div className="MainSectionItem-imageContainerInner">
-//           <div className="AppImage MainSectionItem-image">
-//             <div className="AppImage-image --placeholder --lazy --loaded" style={{ objectFit: 'cover', backgroundImage: "url('/images/test/1.jpg')", backgroundSize: 'cover', backgroundPosition: 'center center', }} ></div>
-//             <img src="/images/test/1.jpg" width="480" height="348" alt="Video of a landscape" loading="lazy" style={{ objectFit: 'cover' }}></img>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   </div>
-// </div>
-// <div className="MainSectionItem MainSection-item">
-//   <div className="--index-between MainSectionItem-inner">
-//     <div className="MainSectionItem-innerSticky">
-//       <div className="MainSectionItem-background" style={{ backgroundColor: 'var(--pink)', }} />
-//       <div className="MainSectionItem-content">
-//         <span className="MainSectionItem-index">02</span>
-//         <h3 className="MainSectionItem-contentTitle AppTitle-3">Nature's Symphony: The Sounds That Heal the Soul</h3>
-//         <div className="MainSectionItem-contentText">
-//           <div className="AppText-12 ">
-//             <p>Immerse yourself in the soothing sounds of chirping birds, rustling leaves, and flowing streams - nature's music for peace and tranquility.</p>
-//           </div>
-//         </div>
-//       </div>
-//       <div className="MainSectionItem-imageContainer">
-//         <div className="MainSectionItem-imageContainerInner">
-//           <div className="AppImage MainSectionItem-image">
-//             <div className="AppImage-image --placeholder --lazy --loaded" style={{ objectFit: 'cover', backgroundImage: "url('/images/test/2.jpg')", backgroundSize: 'cover', backgroundPosition: 'center center', }} ></div>
-//             <img src="/images/test/2.jpg" width="480" height="348" alt="Video of a landscape" loading="lazy" style={{ objectFit: 'cover' }}></img>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   </div>
-// </div>
-// <div className="MainSectionItem MainSection-item">
-//   <div className="--index-between MainSectionItem-inner">
-//     <div className="MainSectionItem-innerSticky">
-//       <div className="MainSectionItem-background" style={{ backgroundColor: 'var(--green)', }} />
-//       <div className="MainSectionItem-content">
-//         <span className="MainSectionItem-index">03</span>
-//         <h3 className="MainSectionItem-contentTitle AppTitle-3">Nature's Masterpieces: Landscapes That Take Your Breath Away</h3>
-//         <div className="MainSectionItem-contentText">
-//           <div className="AppText-12 ">
-//             <p>Discover stunning views of majestic mountains, endless oceans, and golden sunsets that remind us of nature's artistic brilliance.</p>
-//           </div>
-//         </div>
-//       </div>
-//       <div className="MainSectionItem-imageContainer">
-//         <div className="MainSectionItem-imageContainerInner">
-//           <div className="AppImage MainSectionItem-image">
-//             <div className="AppImage-image --placeholder --lazy --loaded" style={{ objectFit: 'cover', backgroundImage: "url('/images/test/3.jpg')", backgroundSize: 'cover', backgroundPosition: 'center center', }} ></div>
-//             <img src="/images/test/3.jpg" width="480" height="348" alt="Video of a landscape" loading="lazy" style={{ objectFit: 'cover' }}></img>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   </div>
-// </div>
