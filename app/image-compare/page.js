d626c861ae91e3df02a2b057dd04261cfa0ab3e9
Wanyi Ng <wanyi.ng1975@gmail.com>
2025-09-20 12:46:07 -0400

    wip base image comparison effect

--- app/image-compare/page.js
+++ app/image-compare/page.js
@@ -7,104 +7,59 @@ import { Draggable } from 'gsap/Draggable'
 
 gsap.registerPlugin(ScrollTrigger, Draggable)
 
-const TestComponent = () => {
-  const barRef = useRef(null)
-  const handlerRef = useRef(null)
-  
+const MainComponent = () => {
+  const after = useRef(null)
+  const afterImg = useRef(null)
+
   useEffect(() => {
     let barLength = 0
     let maxScroll = 0
-    let trigger
-    let draggable
-  
-    function updateHandler() {
-      if (handlerRef.current && trigger) {
-        gsap.set(handler, {
-          x: barLength * trigger.scroll() / maxScroll
-        })
-      }
-    }
-  
-    function onResize() {
-      if (barRef.current && handlerRef.current) {
+    let trigger, draggable
+
+    const onResize = () => {
+      if (trigger) {
         maxScroll = ScrollTrigger.maxScroll(window)
-        barLength = document.querySelector('.bar').offsetWidth - handler.offsetWidth
+        barLength = after.current.offsetWidth - afterImg.current.offsetWidth
         updateHandler()
       }
     }
-  
+    const updateHandler = () => {
+      gsap.set(afterImg.current, {
+        x: barLength * trigger.scroll() / maxScroll
+      })
+    }
+
     const ctx = gsap.context(() => {
       trigger = ScrollTrigger.create({
         onRefresh: onResize,
         onUpdate: updateHandler,
       })
-  
-      if (handlerRef.current && barRef.current) {
-        draggable = Draggable.create(handlerRef.current, {
-          type: 'x',
-          bounds: '.bar',
-          onDrag: function() {
-            trigger.scroll(this.x / barLength * maxScroll)
-          }
-        })[0]
-      }
-  
-      onResize()
+
+      draggable = Draggable.create(afterImg.current, {
+        type: 'x',
+        bounds: after.current,
+        onDrag: function() {
+          trigger.scroll(this.x / barLength * maxScroll)
+        }
+      })[0]
     })
-    
-    return () => {
-      ctx.revert()
-      trigger?.kill()
-      draggable?.kill()
-    }
+
+    return () => ctx.revert()
   }, [])
-  
+
   return (
     <>
-      <div className='bar' ref={barRef}>
-        <div id='handler' ref={handlerRef} />
-      </div>
-    
-      <div className='h-[2000px]' />
-    </>
-  )
-}
-
-const MainComponent = () => {
-  
-  useEffect(() => {
-    const ctx = gsap.context(() => {
-      let tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: '.ImageComparison-container',
-          start: 'top top',
-          end: '+=100%',
-          pin: true,
-          scrub: 1,
-          markers: true,
-        },
-        defaults: { ease: 'none' },
-      })
-      // tl.fromTo('.ImageComparison-before', { xPercent: 0, x: 0 }, { xPercent: 100 }, 0)
-        // .fromTo('.ImageComparison-before img', { xPercent: 0, x: 0 }, { xPercent: -100 }, 0)
-      tl.fromTo('.ImageComparison-after', { xPercent: 100, x: 0 }, { xPercent: 0 }, 0)
-        .fromTo('.ImageComparison-after img', { xPercent: -100, x: 0 }, { xPercent: 0 }, 0)
-    })
-    
-    return () => {
-      ctx.revert()
-    }
-  }, [])
-
-  return (
+    <div className='h-screen w-full' />
     <div className='ImageComparison-container'>
-      <div className='ImageComparison-before'>
+      {/* <div className='ImageComparison-before'>
         <img src='/images/test/base.jpg' alt='Before stage' />
-      </div>
-      <div className='ImageComparison-after'>
-        <img src='/images/test/base.jpg' alt='After stage' />
+      </div> */}
+      <div ref={after} className='ImageComparison-after'>
+        <img ref={afterImg} src='/images/test/base.jpg' alt='After stage' />
       </div>
     </div>
+    <div className='h-screen w-full' />
+    </>
   )
 }
 
