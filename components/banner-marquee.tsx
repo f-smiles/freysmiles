3fc62526f16922b89a3de769847efb120663f1c5
Wanyi Ng <wanyi.ng1975@gmail.com>
2026-02-05 20:03:30 -0500

    update banner

--- components/banner-marquee.tsx
+++ components/banner-marquee.tsx
@@ -1,6 +1,6 @@
 export const BannerMarquee = () => {
-  const text =
-    "Reserve an appointment now to experience our new year courtesy of up to 700 dollars off full treatment.";
+  // const text = "Reserve an appointment now to experience our new year courtesy of up to 700 dollars off full treatment.";
+  const texts = ["Two Smiles. One Price.", "Allentown", "Bethlehem", "Lehighton", "Schnecksville", "Book a Consultation."]
   const repeatCount = 12;
 
   return (
@@ -9,11 +9,14 @@ export const BannerMarquee = () => {
         <div className="marqueegroup">
           {Array.from({ length: repeatCount }).map((_, i) => (
             <div key={`a-${i}`} className="flex items-center">
-              <span className="px-6 py-2 text-[12px] font-neuehaas45 whitespace-nowrap tracking-wide">
-                {text}
-              </span>
-
-              <span className="mx-4 text-[12px] font-light opacity-50">+</span>
+              {texts.map((text, j) => (
+                <div key={`a-${i}-${j}`}>
+                  <span className="px-6 py-2 text-[12px] font-neuehaas45 whitespace-nowrap tracking-wide">
+                    {text}
+                  </span>
+                  <span className="mx-4 text-[12px] font-light opacity-50">+</span>
+                </div>
+              ))}
             </div>
           ))}
         </div>
@@ -21,11 +24,14 @@ export const BannerMarquee = () => {
         <div className="marqueegroup">
           {Array.from({ length: repeatCount }).map((_, i) => (
             <div key={`a-${i}`} className="flex items-center">
-              <span className="px-6 py-2 text-[12px] font-neuehaas45 whitespace-nowrap tracking-wide">
-                {text}
-              </span>
-
-              <span className="mx-4 text-[12px] font-light opacity-50">+</span>
+              {texts.map((text, j) => (
+                <div key={`b-${i}-${j}`}>
+                  <span className="px-6 py-2 text-[12px] font-neuehaas45 whitespace-nowrap tracking-wide">
+                    {text}
+                  </span>
+                  <span className="mx-4 text-[12px] font-light opacity-50">+</span>
+                </div>
+              ))}
             </div>
           ))}
         </div>
