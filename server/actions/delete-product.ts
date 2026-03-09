a4068b081e6e17aba888c22fc57478b9ad069ab3
Wanyi Ng <wanyi.ng1975@gmail.com>
2026-03-09 00:09:49 -0400

    update delete product action to also delete images from uploadthing

--- server/actions/delete-product.ts
+++ server/actions/delete-product.ts
@@ -5,15 +5,38 @@ import { z } from "zod"
 import { eq } from "drizzle-orm"
 import { revalidatePath } from "next/cache"
 import { db } from "../db"
-import { products } from "../schema"
+import { products, productVariants, variantImages } from "../schema"
+import { utapiDeleteFiles } from "./uploadthing"
 
 export const deleteProduct = actionClient
   .schema(z.object({ id: z.number() }))
   .action(async ({ parsedInput: { id } }) => {
     try {
-      const product = await db.delete(products).where(eq(products.id, id)).returning()
+      
+      const product = await db.query.products.findFirst({
+        where: eq(products.id, id),
+        with: {
+          productVariants: {
+            with: {
+              variantImages: true,
+            }
+          }
+        }
+      })
+
+      if (!product) return
+
+      product.productVariants.forEach((productVariant) => {
+        productVariant.variantImages.forEach(async (variantImage) => {
+          await utapiDeleteFiles(variantImage.url.split('/f/')[1])
+        })
+      })
+
+      await db.delete(products).where(eq(products.id, id)).returning()
+
       revalidatePath("/dashboard/products")
-      return { success: `Successfully deleteProduct product - ${product[0].title}` }
+      
+      return { success: `Successfully deleteProduct product - ${product.title}` }
     } catch (error) {
       return { error: "Failed to delete product. Please try again." }
     }
--- server/actions/delete-variant.ts
+++ server/actions/delete-variant.ts
@@ -5,15 +5,30 @@ import { z } from "zod"
 import { eq } from "drizzle-orm"
 import { revalidatePath } from "next/cache"
 import { db } from "../db"
-import { productVariants } from "../schema"
+import { productVariants, variantImages } from "../schema"
+import { utapiDeleteFiles } from "./uploadthing"
 
 export const deleteVariant = actionClient
   .schema(z.object({ id: z.number() }))
   .action(async ({ parsedInput: { id } }) => {
     try {
-      const productVariant = await db.delete(productVariants).where(eq(productVariants.id, id)).returning()
+      
+      const productVariant = await db.query.productVariants.findFirst({
+        where: eq(productVariants.id, id),
+        with: { variantImages: true },
+      })
+
+      if (!productVariant) return
+      
+      productVariant?.variantImages.forEach(async (variantImage) => {
+        await utapiDeleteFiles(variantImage.url.split('/f/')[1])
+      })
+
+      await db.delete(productVariants).where(eq(productVariants.id, id)).returning()
+      
       revalidatePath("/dashboard/products")
-      return { success: `Successfully deleteProduct product - ${productVariant[0].variantName}` }
+      
+      return { success: `Successfully deleteProduct product - ${productVariant.variantName}` }
     } catch (error) {
       return { error: "Failed to delete product. Please try again." }
     }
