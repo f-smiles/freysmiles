cab3fd147d4650eec35fcba665bbecbe9f6812b3
Wanyi Ng <wanyi.ng1975@gmail.com>
2026-03-09 00:05:37 -0400

    update product schema

--- types/product-schema.ts
+++ types/product-schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 
 export const ProductSchema = z.object({
   id: z.number().optional(),
-  title: z.string().min(5, { message: "Title must be at least 5 characters long." }),
+  title: z.string().min(4, { message: "Title must be at least 4 characters long." }),
   description: z.string().min(10, { message: "Description must be at least 10 characters long." }),
   price: z.coerce.number({ invalid_type_error: "Price must be a number" }).positive({ message: "Price must be a positive number." }),
 })
