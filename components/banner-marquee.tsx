6e71dd236eaeba5d6b5c61b2d34c582ed9af9866
Wanyi Ng <wanyi.ng1975@gmail.com>
2026-03-20 17:15:22 -0400

    only show banner on landing

--- components/banner-marquee.tsx
+++ components/banner-marquee.tsx
@@ -1,42 +1,51 @@
+"use client"
+import { usePathname } from "next/navigation";
+
 export const BannerMarquee = () => {
   // const text = "Reserve an appointment now to experience our new year courtesy of up to 700 dollars off full treatment.";
   const texts = ["Allentown", "Bethlehem", "Lehighton", "Schnecksville", "Complimentary Consultation"]
   const repeatCount = 12;
 
-  return (
-    // Banner colors: #ABF926, #00F0FF, #9bf3f3
-    <div className="relative w-full overflow-hidden bg-[#9bf3f3]">
-      <div className="marquee">
-        <div className="marqueegroup">
-          {Array.from({ length: repeatCount }).map((_, i) => (
-            <div key={`a-${i}`} className="flex items-center">
-              {texts.map((text, j) => (
-                <div key={`a-${i}-${j}`}>
-                  <span className="px-6 py-2 text-[12px] font-neuehaas45 whitespace-nowrap tracking-wide">
-                    {text}
-                  </span>
-                  <span className="mx-4 text-[12px] font-light opacity-50">+</span>
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
+  const pathname = usePathname()
 
-        <div className="marqueegroup">
-          {Array.from({ length: repeatCount }).map((_, i) => (
-            <div key={`a-${i}`} className="flex items-center">
-              {texts.map((text, j) => (
-                <div key={`b-${i}-${j}`}>
-                  <span className="px-6 py-2 text-[12px] font-neuehaas45 whitespace-nowrap tracking-wide">
-                    {text}
-                  </span>
-                  <span className="mx-4 text-[12px] font-light opacity-50">+</span>
+  return (
+    <>
+      {pathname !== "/" ? null : (
+        // Banner colors: #ABF926, #00F0FF, #9bf3f3
+        <div className="relative w-full overflow-hidden bg-[#9bf3f3]">
+          <div className="marquee">
+            <div className="marqueegroup">
+              {Array.from({ length: repeatCount }).map((_, i) => (
+                <div key={`a-${i}`} className="flex items-center">
+                  {texts.map((text, j) => (
+                    <div key={`a-${i}-${j}`}>
+                      <span className="px-6 py-2 text-[12px] font-neuehaas45 whitespace-nowrap tracking-wide">
+                        {text}
+                      </span>
+                      <span className="mx-4 text-[12px] font-light opacity-50">+</span>
+                    </div>
+                  ))}
                 </div>
               ))}
             </div>
-          ))}
+    
+            <div className="marqueegroup">
+              {Array.from({ length: repeatCount }).map((_, i) => (
+                <div key={`a-${i}`} className="flex items-center">
+                  {texts.map((text, j) => (
+                    <div key={`b-${i}-${j}`}>
+                      <span className="px-6 py-2 text-[12px] font-neuehaas45 whitespace-nowrap tracking-wide">
+                        {text}
+                      </span>
+                      <span className="mx-4 text-[12px] font-light opacity-50">+</span>
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  );
+      )}
+    </>
+  )
 };
