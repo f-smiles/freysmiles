e8513d8e55a6679d55aa644790a13839cf7bca07
Wanyi Ng <wanyi.ng1975@gmail.com>
2026-03-20 14:30:33 -0400

    update fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
 				'neuehaas45': ['NeueHaasGroteskDisplayPro45Light', 'sans-serif'],
 				'canela': ['CanelaThin', 'serif'],
 				'canela-italic': ['CanelaThinItalic', 'serif'],
+				'ibmplex': ['IBMPlexMono', 'monospace'],
 			},
   		borderRadius: {
   			lg: 'var(--radius)',
